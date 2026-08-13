4c56b6546c6514c5c98119f6c4f3b2044152c96e
akuncadanganguys2026-debug <akuncadanganguys2026@gmail.com>
2026-08-13 09:59:27 +0700

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,32 +1,39 @@
-# ==========================================
-#        PROGRAM BELAJAR PYTHON STRING
-# ==========================================
+==========================================
+
+PROGRAM BELAJAR PYTHON STRING
+
+==========================================
 
 print("=" * 45)
 print("       PROGRAM DATA DIRI SEDERHANA")
 print("=" * 45)
 
-# Input data
-nama = input("Masukkan nama  : ")
-kelas = input("Masukkan kelas : ")
-sekolah = input("Masukkan sekolah: ")
+Data diri
 
-# Mengolah string
+nama = "Ahmad Fauzan"
+kelas = "10 TKJ A"
+sekolah = "Bina Mandiri"
+
+Mengolah string
+
 nama_besar = nama.upper()
 nama_kecil = nama.lower()
 
-# Menampilkan hasil
+Menampilkan hasil
+
 print("\n" + "=" * 45)
 print("             HASIL DATA DIRI")
 print("=" * 45)
 
-print(f"Nama lengkap : {Ahmad Fauzan }")
-print(f"Huruf besar  : {AHMAD FAUZAN}")
-print(f"Huruf kecil  : {Ahmad fauzan}")
-print(f"Kelas        : {10 TKJ A}")
-print(f"Sekolah      : {bina mandiri}")
+print(f"Nama lengkap : {nama}")
+print(f"Huruf besar  : {nama_besar}")
+print(f"Huruf kecil  : {nama_kecil}")
+print(f"Kelas        : {kelas}")
+print(f"Sekolah      : {sekolah}")
 print(f"Panjang nama : {len(nama)} karakter")
 
 print("=" * 45)
 print("      Terima kasih sudah mencoba!")
 print("=" * 45)
+
+jadi gmn kalo d jalanin di python
